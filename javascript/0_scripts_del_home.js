f891144c674e4ecca4ac7121dee1d52ae6f1aea1
est.diana.camargo3 <est.diana.camargo3@unimilitar.edu.co>
2025-04-26 12:06:25 -0500

    Re organizacion del home en 3 archivos separados : html, css, js

---
+++ javascript/0_scripts_del_home.js
@@ -0,0 +1,16 @@
+/* Javascript - JS */
+/* Aqui estan todas las aplicaciones, efectos animaciones validaciones de formularios ...*/
+/* Es decir que al tocar esta pared prenda un bombillo...y asi con todas sus interacciones  */ 
+/*aqui encontraremos "todo" en funciones, con condicionales, whiles, ifs...que se ejecutaran cuando "un boton" se presione
+o cuando algo pase*/
+
+/*
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓                                                                                                                           ▓▓
+▓▓                              J S    -  J A V A S C R I P T     - I N T E R A C C I O N E S                                ▓▓
+▓▓                                                                                                                           ▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ */
+
+
+
+  
