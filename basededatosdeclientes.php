ee0bccccf48b644c669073d24c94330b1203ced6
est.diana.camargo3 <est.diana.camargo3@unimilitar.edu.co>
2025-05-13 07:51:24 -0500

    creacion de carpeta php, json y xml

---
+++ basededatosdeclientes.php
@@ -0,0 +1,13 @@
+<?php
+header("Content-Type:application/json;charset=UTF-8")
+/* Tendremos una base de datos que va a estar en mySQL
+    haremos el acceso a ella por medio de PHP 
+    que se hara de manera asincrona usando AJAX
+    nos dara como resultado del acceso un JSON o cadena
+    el cual parsearemos para poderlo mostrar en JAVASCRIPT Y HTML 
+    */
+
+    /*Mi base de datos tiene 
+      cliente, contraseña , tipo de cuenta y  foto */
+
+?>
