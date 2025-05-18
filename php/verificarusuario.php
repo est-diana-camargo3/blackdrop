3c9f135924eb1b6603bfd226bbcc5600719472f5
Juanita Palacios <est.juanita.palaci@unimilitar.edu.co>
2025-05-17 22:12:01 -0500

    login try1

---
+++ php/verificarusuario.php
@@ -0,0 +1,23 @@
+<?php
+header("Content-Type: application/json");
+include("conexion.php"); // Asegúrate de tener un archivo con la conexión a la base de datos
+
+$correo = $_POST['correo'];
+$contrasena = $_POST['contrasena'];
+$tipodeusuario = $_POST['tipodeusuario'];
+
+// Buscar si el usuario existe
+$query = "SELECT correo, tipodeusuario FROM usuarios22 WHERE correo = $1 AND contrasena = $2 AND tipodeusuario = $3";
+$resultado = pg_query_params($conn, $query, array($correo, $contrasena, $tipoCuenta));
+
+if (pg_num_rows($resultado) > 0) {
+    $usuario = pg_fetch_assoc($resultado);
+    echo json_encode([
+        "exito" => true,
+        "correo" => $usuario['correo'],
+        "redireccion" => $usuario['tipodeusuario'] == "cliente" ? "../html/4paginacliente.html" : "../html/3paginaadministrador.html"
+    ]);
+} else {
+    echo json_encode(["exito" => false]);
+}
+?>
