09ff17c3d3e998c8fb5a79282159d5b8ca345af2
est.diana.camargo3 <est.diana.camargo3@unimilitar.edu.co>
2025-05-21 11:38:24 -0500

    intento 10 de configuracion de usuario incorrecto

--- php/verificarusuario.php
+++ php/verificarusuario.php
@@ -11,43 +11,43 @@ $tipodeusuario = $_POST['tipodeusuario'];
 $query = "SELECT correo, tipodeusuario FROM usuarios22 WHERE correo = $1 AND contrasena = $2 AND tipodeusuario = $3";
 $resultado = pg_query_params($conn, $query, array($correo, $contrasena, $tipodeusuario));
 
+// Si el usuario no existe 
 if (!$resultado) {
     die("❌ Error en la consulta SQL: " . pg_last_error($conn));
 }
 
-
-
+// Si el usuario si existe
 if (pg_num_rows($resultado) > 0) 
     {
         $usuario = pg_fetch_assoc($resultado);
 
         // ✅ Redirección según el tipo de usuario
         if ($usuario['tipodeusuario'] === 'cliente') 
-        {
-            header("Location: ../html/4paginacliente.html");
-            $respuesta = [
-            "exito" => true,
-            "correo" => $usuario['correo'],
-            "mensaje" => "❌ Autenticacion de cliente correcta"];
-            
-            exit;
-        } elseif ($usuario['tipodeusuario'] === 'administrador') 
-        {
-            header("Location: ../html/3paginaadministrador.html");
-            $respuesta = [
-            "exito" => true,
-            "correo" => $usuario['correo'],
-            "mensaje" => "❌ Autenticacion de administrador correcta"];
-           
-            exit;
-        }
-        
+                {
+                    header("Location: ../html/4paginacliente.html");
+                    $respuesta = [
+                    "exito" => true,
+                    "correo" => $usuario['correo'],
+                    "mensaje" => "❌ Autenticacion de cliente correcta"];
+                    
+                    exit;
+                } 
+        else if ($usuario['tipodeusuario'] === 'administrador') 
+                {
+                    header("Location: ../html/3paginaadministrador.html");
+                    $respuesta = [
+                    "exito" => true,
+                    "correo" => $usuario['correo'],
+                    "mensaje" => "❌ Autenticacion de administrador correcta"];
+                
+                    exit;
+                }        
     } 
 else 
     $respuesta = [
-        "exito" => false,
-        "mensaje" => "❌ Usuario o contraseña incorrectos"
-    ];
+                    "exito" => false,
+                    "mensaje" => "❌ Usuario o contraseña incorrectos"
+                 ];
     header("Location: ../html/2indexdellogin.html");
 
     
