973f5e9014009f87662eccb4078db70d7a34ee14
Juanita Palacios <est.juanita.palaci@unimilitar.edu.co>
2025-05-25 17:17:51 -0500

    try3

--- php/2verificarusuario.php
+++ php/2verificarusuario.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 // Mostrar errores en desarrollo
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -35,4 +36,5 @@ if ($result && pg_num_rows($result) === 1) {
 }
 
 pg_close($conn);
+ob_clean();
 ?>
